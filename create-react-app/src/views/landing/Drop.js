8dac441393d731be25f7d5831e61b45f9a22c267
Lam Uong <lam.uong2@mail.mcgill.ca>
2023-05-26 18:33:52 -0700

    Create Drop.js

---
+++ create-react-app/src/views/landing/Drop.js
@@ -0,0 +1,36 @@
+import React, { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+
+export default function Drop({ onLoaded }) {
+  const styles = {
+    container: {
+      textAlign: "center",
+      border: "5px",
+      padding: 20,
+      marginTop: 12,
+      color: "hsl(218,49%,66%)",
+      fontSize: 18,
+      fontWeight: 600,
+      borderRadius: 4,
+      userSelect: "none",
+      outline: 0,
+      cursor: "pointer",
+    },
+  };
+
+  const onDrop = useCallback((acceptedFiles) => {
+    onLoaded(acceptedFiles);
+    // Do something with the files
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: "application/pdf",
+  });
+
+  return (
+    <div {...getRootProps()} style={styles.container}>
+      <input {...getInputProps()} />
+      {isDragActive ? <p>Drop a PDF here</p> : <p>Drag a PDF here</p>}
+    </div>
+  );
+}
