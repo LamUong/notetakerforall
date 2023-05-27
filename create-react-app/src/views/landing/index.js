bfb47d33e66c2973ea746aa93f6466a28f793106
Lam Uong <lam.uong2@mail.mcgill.ca>
2023-05-26 18:16:56 -0700

    Update index.js

--- create-react-app/src/views/landing/index.js
+++ create-react-app/src/views/landing/index.js
@@ -42,7 +42,7 @@ const Landing = () => {
                             Automatically get transcript, summaries, highlights and bullet points without hassel. Ask questions
                             and answers from your notes powered by ChatGPT.
                           </Typography>
-                          <Button variant="contained" endIcon={<MicIcon />} marginTop="30px !important" >
+                          <Button variant="contained" endIcon={<MicIcon />} sx={{ marginTop: '30px !important' }}>
                             Start Recording
                           </Button>
                           <Grid item xs={12} width="100%" >
