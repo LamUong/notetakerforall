6dcb4bfa80d8ff32eeb4b5ab59b90ebda2abc178
Lam Uong <lam.uong2@mail.mcgill.ca>
2023-05-27 10:55:13 -0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,73 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, WebSocket
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from typing import Dict, Callable
+from deepgram import Deepgram
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "FastAPI"}
+dg_client = Deepgram(os.getenv('DEEPGRAM_API_KEY'))
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+templates = Jinja2Templates(directory="templates")
 
+async def process_audio(fast_socket: WebSocket):
+    async def get_transcript(data: Dict) -> None:
+        if 'channel' in data:
+            transcript = data['channel']['alternatives'][0]['transcript']
+        
+            if transcript:
+                await fast_socket.send_text(transcript)
+
+    deepgram_socket = await connect_to_deepgram(get_transcript)
+
+    return deepgram_socket
+
+async def connect_to_deepgram(transcript_received_handler: Callable[[Dict], None]):
+    try:
+        socket = await dg_client.transcription.live({'punctuate': True, 'interim_results': False, 'model': 'nova'})
+        socket.registerHandler(socket.event.CLOSE, lambda c: print(f'Connection closed with code {c}.'))
+        socket.registerHandler(socket.event.TRANSCRIPT_RECEIVED, transcript_received_handler)
+        
+        return socket
+    except Exception as e:
+        raise Exception(f'Could not open socket: {e}')
+ 
+@app.get("/", response_class=HTMLResponse)
+def get(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.websocket("/listen")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+
+    try:
+        deepgram_socket = await process_audio(websocket) 
+
+        while True:
+            data = await websocket.receive_bytes()
+            deepgram_socket.send(data)
+    except Exception as e:
+        raise Exception(f'Could not process audio: {e}')
+    finally:
+        await websocket.close()
+ 
+@app.get(
+    path="/upload"
+)
+async def transcribe_audio_file(file: UploadFile):
+    """
+    Receive File, store to disk & return it
+    """
+    # Write file to disk. This simulates some business logic that results in a file sotred on disk
+    with open(os.path.join(tmp_file_dir, file.filename), 'wb') as disk_file:
+        file_bytes = await file.read()
+
+        disk_file.write(file_bytes)
+
+        print(f"Received file named {file.filename} containing {len(file_bytes)} bytes. ")
+
+        return FileResponse(disk_file.name, media_type=file.content_type)
