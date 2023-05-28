e54bdf8fdea545de7b8bea8ab9286183ce48c194
Lam Uong <lam.uong2@mail.mcgill.ca>
2023-05-28 13:32:40 -0700

    Update index.js

--- create-react-app/src/views/sample-page/index.js
+++ create-react-app/src/views/sample-page/index.js
@@ -7,14 +7,18 @@ import MainCard from 'ui-component/cards/MainCard';
 const SamplePage = () => {
   const [progress, setProgress] = useState(0);
   const [isUploading, setIsUploading] = useState(false);
+  const [isTranscribing, setIsTranscribing] = useState(false);
+  const [isUploaded, setIsUploaded] = useState(false);
+  const [transcript, setTranscript] = useState("");
+
   const location = useLocation();
   const maybeVideo = location.state.file;
 
   const handleUpload = async () => {
     const formData = new FormData();
     formData.append('file', maybeVideo);
-
     try {
+      setIsUploading(true);
       const response = await axios.post('http://3.125.247.51:8000/upload_file', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
@@ -22,25 +26,50 @@ const SamplePage = () => {
         onUploadProgress: (progressEvent) => {
           const progress = Math.round((progressEvent.loaded / progressEvent.total) * 100);
           setProgress(progress);
+          if (progress == 100){
+            setIsUploading(false);
+            setIsTranscribing(true);
+          }
         },
       });
       console.log('Video uploaded successfully:', response.data);
+      setTranscript(response.data);
     } catch (error) {
       console.error('Error uploading video:', error);
+    } finally {
+      setIsUploading(false);
+      setIsTranscribing(false);
+      isUploaded(true);
     }
   };
 
   useEffect(() => {
-    if (maybeVideo && !isUploading) {
-      setIsUploading(true);
+    if (maybeVideo && !isUploaded) {
       handleUpload();
     }
-  }, [maybeVideo, isUploading]);
+  }, [maybeVideo, isUploaded]);
 
   return (
     <MainCard title="Sample Card">
-      <Typography variant="body2">Upload Progress</Typography>
-      <LinearProgress variant="determinate" value={progress} />
+      {isUploading &&
+        <div>
+          <Typography variant="body2">Upload Progress</Typography>
+          <LinearProgress variant="determinate" value={progress} />
+        </div>
+      }
+      {isTranscribing &&
+        <div>
+          <Typography variant="body2">Upload Progress</Typography>
+          <LinearProgress variant="determinate" value={progress} />
+        </div>
+      }
+      {isUploaded &&
+        <div>
+          {transcript}
+        </div>
+      }
+
+      
       <Typography variant="body2">
         Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif ad
         minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in reprehended
