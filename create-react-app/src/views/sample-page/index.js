d9309c5627278c20aef5e44a92d79d1677b73a59
Lam Uong <lam.uong2@mail.mcgill.ca>
2023-05-28 12:10:47 -0700

    Update index.js

--- create-react-app/src/views/sample-page/index.js
+++ create-react-app/src/views/sample-page/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Typography } from '@mui/material';
+import { Typography, LinearProgress } from '@mui/material';
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
 import MainCard from 'ui-component/cards/MainCard';
