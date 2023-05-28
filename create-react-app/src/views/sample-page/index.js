2b427920acecbb7812403e4f79bcca98dd04d6b7
Lam Uong <lam.uong2@mail.mcgill.ca>
2023-05-28 11:33:33 -0700

    Update index.js

--- create-react-app/src/views/sample-page/index.js
+++ create-react-app/src/views/sample-page/index.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { Typography } from '@mui/material';
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
@@ -6,6 +7,7 @@ import MainCard from 'ui-component/cards/MainCard';
 // ==============================|| SAMPLE PAGE ||============================== //
 
 const SamplePage = () => {
+  const [uploadProgress, setUploadProgress] = useState(0);
   const location = useLocation();
   const maybeVideo = location.state.file;
   if (maybeVideo) {
@@ -15,6 +17,12 @@ const SamplePage = () => {
       axios.post('http://3.125.247.51:8000/upload_file', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
+        },
+        onUploadProgress: (progressEvent) => {
+          const progress = Math.round(
+            (progressEvent.loaded * 100) / progressEvent.total
+          );
+          setUploadProgress(progress);
         },
       })
       .then((response) => {
@@ -26,6 +34,7 @@ const SamplePage = () => {
   }
   return (
       <MainCard title="Sample Card">
+        <Typography variant="body2">Progress: {uploadProgress}%</Typography>
         <Typography variant="body2">
           Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif ad
           minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in reprehended
