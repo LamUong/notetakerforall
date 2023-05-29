ee1647509fa4c44f93808a3a86b2324671ce3056
Lam Uong <lam.uong2@mail.mcgill.ca>
2023-05-28 18:41:48 -0700

    Update index.js

--- create-react-app/src/views/sample-page/index.js
+++ create-react-app/src/views/sample-page/index.js
@@ -27,14 +27,12 @@ const SamplePage = () => {
           const progress = Math.round((progressEvent.loaded / progressEvent.total) * 100);
           setProgress(progress);
           if (progress == 100){
-            console.log("transcribing");
             setIsUploading(false);
             setIsTranscribing(true);
           }
         },
       });
-      console.log('Video uploaded successfully:', response.data);
-      setTranscript(response.data);
+      setTranscript(response.data.transcript_with_ts);
     } catch (error) {
       console.error('Error uploading video:', error);
     } finally {
@@ -70,12 +68,6 @@ const SamplePage = () => {
           {transcript}
         </div>
       }
-      <Typography variant="body2">
-        Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif ad
-        minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in reprehended
-        in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa qui officiate
-        descent molls anim id est labours.
-      </Typography>
     </MainCard>
   );
 };
