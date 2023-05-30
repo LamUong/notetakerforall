a335f2558297ffff96ab93f097521bfdc0ba0ea6
Lam Uong <lam.uong2@mail.mcgill.ca>
2023-05-29 17:35:29 -0700

    Update index.js

--- create-react-app/src/views/sample-page/index.js
+++ create-react-app/src/views/sample-page/index.js
@@ -4,6 +4,86 @@ import { useLocation } from 'react-router-dom';
 import axios from 'axios';
 import MainCard from 'ui-component/cards/MainCard';
 
+const VideoUpload = (props) => {
+  const [progress, setProgress] = useState(0);
+  const [isUploading, setIsUploading] = useState(false);
+  const [isTranscribing, setIsTranscribing] = useState(false);
+  const [isUploaded, setIsUploaded] = useState(false);
+  const [transcript, setTranscript] = useState("");
+  const video = props.file;
+
+  const handleUpload = async () => {
+    const formData = new FormData();
+    formData.append('file', video);
+    try {
+      setIsUploading(true);
+      const response = await axios.post('http://3.125.247.51:8000/mock_upload_file', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+        onUploadProgress: (progressEvent) => {
+          const progress = Math.round((progressEvent.loaded / progressEvent.total) * 100);
+          setProgress(progress);
+          if (progress == 100){
+            setIsUploading(false);
+            setIsTranscribing(true);
+          }
+        },
+      });
+      console.log(response.data);
+      setTranscript(response.data.transcript_with_ts);
+    } catch (error) {
+      console.error('Error uploading video:', error);
+    } finally {
+      console.log("isUploaded=true");
+      setIsUploading(false);
+      setIsTranscribing(false);
+      setIsUploaded(true);
+    }
+  };
+
+  useEffect(() => {
+    if (!isUploaded) {
+      handleUpload();
+    }
+  }, [isUploaded]);
+
+  return (
+    <MainCard title="Transcript">
+      {isUploading &&
+        <div>
+          <Typography variant="body2">Upload Progress</Typography>
+          <LinearProgress variant="determinate" value={progress} />
+        </div>
+      }
+      {isTranscribing &&
+        <div>
+          <Typography variant="body2">Transcribing</Typography>
+          <CircularProgress />
+        </div>
+      }
+      {isUploaded &&
+        <div style={{whiteSpace: "pre-line"}}>
+          {transcript}
+        </div>
+      }
+    </MainCard>
+  );
+};
+
+const SamplePage = () => {
+  const location = useLocation();
+  const maybeVideo = location.state.file;
+  return (
+    {maybeVideo &&
+       <VideoUpload file={maybeVideo}/>
+    }
+  );
+};
+
+export default SamplePage;
+
+/*
 const SamplePage = () => {
   const [progress, setProgress] = useState(0);
   const [isUploading, setIsUploading] = useState(false);
@@ -74,3 +154,4 @@ const SamplePage = () => {
 };
 
 export default SamplePage;
+*/
