3ea0e725901e8a975e7c7bf425c41c1ef7cebf32
Lam Uong <lam.uong2@mail.mcgill.ca>
2023-05-30 18:59:21 -0700

    Update App.js

--- create-react-app/src/App.js
+++ create-react-app/src/App.js
@@ -2,6 +2,8 @@ import { useSelector } from 'react-redux';
 
 import { ThemeProvider } from '@mui/material/styles';
 import { CssBaseline, StyledEngineProvider } from '@mui/material';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 // routing
 import Routes from 'routes';
@@ -18,14 +20,16 @@ const App = () => {
   const customization = useSelector((state) => state.customization);
 
   return (
-    <StyledEngineProvider injectFirst>
-      <ThemeProvider theme={themes(customization)}>
-        <CssBaseline />
-        <NavigationScroll>
-          <Routes />
-        </NavigationScroll>
-      </ThemeProvider>
-    </StyledEngineProvider>
+    <Provider store={store}>
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={themes(customization)}>
+          <CssBaseline />
+          <NavigationScroll>
+            <Routes />
+          </NavigationScroll>
+        </ThemeProvider>
+      </StyledEngineProvider>
+    </Provider>
   );
 };
 
