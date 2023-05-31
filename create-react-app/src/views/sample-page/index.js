7b764af61bf2b9c681eb032880de7d45f20a34cf
Lam Uong <lam.uong2@mail.mcgill.ca>
2023-05-30 20:36:10 -0700

    Update index.js

--- create-react-app/src/views/sample-page/index.js
+++ create-react-app/src/views/sample-page/index.js
@@ -25,12 +25,12 @@ const ContentTabs = (props) => {
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
           <TabList onChange={handleChange} aria-label="lab API tabs example">
             <Tab label="Transcript" value="1" />
-            <Tab label="Item Two" value="2" />
+            <Tab label="Notes" value="2" />
             <Tab label="Item Three" value="3" />
           </TabList>
         </Box>
         <TabPanel value="1"><VideoTranscript /></TabPanel>
-        <TabPanel value="2">Item Two</TabPanel>
+        <TabPanel value="2">Notes</TabPanel>
         <TabPanel value="3">Item Three</TabPanel>
       </TabContext>
     </Box>
