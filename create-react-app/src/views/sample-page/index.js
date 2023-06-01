1338d5ffb20358c9b32fc2a24d1453bc531d78da
Lam Uong <lam.uong2@mail.mcgill.ca>
2023-05-31 20:22:07 -0700

    Update index.js

--- create-react-app/src/views/sample-page/index.js
+++ create-react-app/src/views/sample-page/index.js
@@ -46,13 +46,13 @@ const Notes = () => {
         />
       </Grid>
       <Grid item xs={4}> 
-        <Box sx={{ width: '100%', border: '1px solid #ccc' }}>
+        <Box sx={{ width: '100%', border: '1px solid #ccc', height: '100%' }}>
           <Container sx={{ my: 3, mx: 2 }}>
             <Typography color="text.secondary" variant="body2">
               Smart Editor
             </Typography>
           </Container>
-          <Divider variant="middle" />
+          <Divider variant="fullWidth" />
           <Box sx={{ m: 2 }}>
             <Typography gutterBottom variant="body1">
               Selected Text
