29c786b4466af024085dd93b172555e9daaca4f2
Lam Uong <lam.uong2@mail.mcgill.ca>
2023-06-02 15:20:47 -0700

    Update index.js

--- create-react-app/src/views/sample-page/index.js
+++ create-react-app/src/views/sample-page/index.js
@@ -85,12 +85,24 @@ const Notes = () => {
       const editor = quillRef.current.getEditor();
       
       const text = editor.getText(range.index, range.length);
-      const delta = editor.getContents(range.index, range.length);
-
+      const delta = editor.getContents(range.index, range.length); 
+      
+      editor.formatText(customization., range.length, {
+        'color': 'red',
+        'background-color': 'blue'
+      }); 
+      
       dispatch({ type: 'SET_HIGHLIGHTED_NOTES', payload: {'text': text, 'delta': delta} });
       dispatch({ type: 'SET_HIGHLIGHTED_NOTES_RANGE', payload: {'index': range.index, 'length': range.length} });
     } else {
       console.log("range.length = 0");
+      const editor = quillRef.current.getEditor();
+      editor.formatText(customization.highlighted_notes_range.index, customization.highlighted_notes_range.length, {
+        'color': 'black',
+        'background-color': 'white'
+      });  
+      
+      
       dispatch({ type: 'SET_HIGHLIGHTED_NOTES', payload: null });
       dispatch({ type: 'SET_HIGHLIGHTED_NOTES_RANGE', payload: null });
     }
