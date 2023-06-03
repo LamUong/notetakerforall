7595e10808cde42d02f397009be8950663982652
Lam Uong <lam.uong2@mail.mcgill.ca>
2023-06-02 20:34:35 -0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -211,7 +211,7 @@ async def stream(websocket: WebSocket):
     # Receive message from the WebSocket
     data = await websocket.receive_text()
     print(f"Received: {data}")
-    data = json.load(data)
+    data = json.loads(data)
 
     answer = ""
     response = get_chat_response(data['input_text'], data['input_type'])
