1aee0357957da2068f672d8ec502924ed012a822
Lam Uong <lam.uong2@mail.mcgill.ca>
2023-06-05 19:58:47 -0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -104,7 +104,7 @@ def generate_outline_html(text):
                 timestamp, description = line.split(',', 1)
                 bullet_point = f'<li>{timestamp.strip()}:{description.strip()}</li>'
                 bullet_points.append(bullet_point)
-            elif len(bullet_point) > 0:
+            elif len(bullet_points) > 0:
                 new_bullet_point = bullet_point[-1].replace('</li>', '') 
                 new_bullet_point.append(f". {line}</li>")
                 bullet_points[-1] = new_bullet_point
