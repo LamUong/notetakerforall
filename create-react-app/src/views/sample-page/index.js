1012bd2159b0731e0661b1eb3507e27d9f37e181
Lam Uong <lam.uong2@mail.mcgill.ca>
2023-06-06 16:47:36 -0700

    Update index.js

--- create-react-app/src/views/sample-page/index.js
+++ create-react-app/src/views/sample-page/index.js
@@ -31,41 +31,22 @@ const MyCard = ({ onReplaceButtonClick }) => {
   }
   
   function copyToClipBoard() {
-    var textArea = document.createElement("textarea");
-    // Place in the top-left corner of screen regardless of scroll position.
-    textArea.style.position = 'fixed';
-    textArea.style.top = 0;
-    textArea.style.left = 0;
-
-    // Ensure it has a small width and height. Setting to 1px / 1em
-    // doesn't work as this gives a negative w/h on some browsers.
-    textArea.style.width = '2em';
-    textArea.style.height = '2em';
-
-    // We don't need padding, reducing the size if it does flash render.
-    textArea.style.padding = 0;
-
-    // Clean up any borders.
-    textArea.style.border = 'none';
-    textArea.style.outline = 'none';
-    textArea.style.boxShadow = 'none';
-
-    // Avoid flash of the white box if rendered for any reason.
-    textArea.style.background = 'transparent';
-    textArea.value = customization.chat_response;
-
-    document.body.appendChild(textArea);
-    textArea.focus();
-    textArea.select();
-    try {
-      var successful = document.execCommand('copy');
-      var msg = successful ? 'successful' : 'unsuccessful';
-      console.log('Copying text command was ' + msg);
-    } catch (err) {
-      console.log('Oops, unable to copy');
-    }
-    document.body.removeChild(textArea);
-  }
+    const htmlContent = customization.chat_response;
+    // Create a temporary element to hold the HTML content
+    const tempElement = document.createElement("div");
+    tempElement.innerHTML = htmlContent;
+    document.body.appendChild(tempElement);
+    // Select the HTML content
+    const range = document.createRange();
+    range.selectNode(tempElement);
+    // Copy the selected HTML content to the clipboard
+    const selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+    document.execCommand("copy");
+    selection.removeAllRanges();
+    document.body.removeChild(tempElement);
+}
   
   const chatStream = (valueType) => {
     dispatch({ type: 'SET_CHAT_ACTION_TYPE', payload: valueType });
