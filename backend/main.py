15724df8db1f17e1a8a83090da8d87d4b614e970
Lam Uong <lam.uong2@mail.mcgill.ca>
2023-06-08 14:52:57 -0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -131,42 +131,25 @@ def format_chat_response(input_text, input_type):
     if input_type == 'Outline':
         return generate_outline_html(input_text) 
     
-async def process_audio(fast_socket: WebSocket):
-    async def get_transcript(data: Dict) -> None:
-        if 'channel' in data:
-            transcript = data['channel']['alternatives'][0]['transcript']
-        
-            if transcript:
-                await fast_socket.send_text(transcript)
-
-    deepgram_socket = await connect_to_deepgram(get_transcript)
-
-    return deepgram_socket
-
-async def connect_to_deepgram(transcript_received_handler: Callable[[Dict], None]):
-    try:
-        socket = await dg_client.transcription.live({'punctuate': True, 'interim_results': False})
-        socket.registerHandler(socket.event.CLOSE, lambda c: print(f'Connection closed with code {c}.'))
-        socket.registerHandler(socket.event.TRANSCRIPT_RECEIVED, transcript_received_handler)
-        
-        return socket
-    except Exception as e:
-        raise Exception(f'Could not open socket: {e}')
-
+async def get_transcript(data: Dict) -> None:
+    print(data)
+                
 @app.websocket("/back_end_listen")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
 
     try:
-        deepgram_socket = await process_audio(websocket) 
-
-        while True:
-            data = await websocket.receive_bytes()
-            deepgram_socket.send(data)
+        deepgramLive = await deepgram.transcription.live({ 'punctuate': True })
     except Exception as e:
-        raise Exception(f'Could not process audio: {e}')
-    finally:
-        await websocket.close()
+        print(f'Could not open socket: {e}')
+        return
+    
+    deepgramLive.registerHandler(deepgramLive.event.CLOSE, lambda c: print(f'Connection closed with code {c}.'))
+    deepgramLive.registerHandler(deepgramLive.event.TRANSCRIPT_RECEIVED, get_transcript)
+    
+    while True:
+        data = await websocket.receive_bytes()
+        deepgramLive.send(data)        
 
 def format_time(timestamp):
     minutes = timestamp // 60
