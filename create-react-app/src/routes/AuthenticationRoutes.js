1111adf8dd9e51ab998ac30a3ba34f2edc1422db
Lam Uong <lam.uong2@mail.mcgill.ca>
2023-06-12 18:49:21 -0700

    Update AuthenticationRoutes.js

--- create-react-app/src/routes/AuthenticationRoutes.js
+++ create-react-app/src/routes/AuthenticationRoutes.js
@@ -8,12 +8,19 @@ import MinimalLayout from 'layout/MinimalLayout';
 const AuthLogin3 = Loadable(lazy(() => import('views/pages/authentication/authentication3/Login3')));
 const AuthRegister3 = Loadable(lazy(() => import('views/pages/authentication/authentication3/Register3')));
 
+
+// landing page routing
+const Landing = Loadable(lazy(() => import('views/landing')));
 // ==============================|| AUTHENTICATION ROUTING ||============================== //
 
 const AuthenticationRoutes = {
   path: '/',
   element: <MinimalLayout />,
   children: [
+    {
+      path: '/',
+      element: <Landing />
+    },
     {
       path: '/pages/login/login3',
       element: <AuthLogin3 />
