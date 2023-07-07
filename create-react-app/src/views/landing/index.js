3338e57bfc024a85b29c7234d9d629f1ea1d1296
Lam Uong <lam.uong2@mail.mcgill.ca>
2023-07-07 16:10:39 -0700

    Update index.js

--- create-react-app/src/views/landing/index.js
+++ create-react-app/src/views/landing/index.js
@@ -9,6 +9,7 @@ import Drop from "./Drop";
 import Logo from 'ui-component/Logo';
 import { useDispatch, useSelector } from 'react-redux';
 import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
+import { fixWebmDuration } from 'fix-webm-duration';
 
 const LogoSection = () => (
   <Grid item sx={{ mb: 3 }}>
@@ -87,8 +88,11 @@ const AudioRecorder = () => {
         console.error("no suitable mimetype found for this device");
     }
     console.log(type);
-    const blob = new Blob(recordedChunksRef.current, { type: type });
-    console.log(blob);
+    let blob = new Blob(recordedChunksRef.current, { type: type });
+    if (type == 'audio/webm'){
+      console.log("fixed_duration");
+      blob = await fixWebmDuration(blob);
+    }
     dispatch({ type: 'SET_IS_RECORDING_AUDIO', payload: false });
     dispatch({ type: 'SET_INPUT_TYPE', payload: 'video' });
     navigate('/sample-page', {state: {file: blob}});
